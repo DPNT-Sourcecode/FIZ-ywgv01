2dfd3477e5b2f6dcffd69be46f755cf3d53968dd
sbx_user1060 <sbx_user1060@ip-10-129-108-47.eu-west-2.compute.internal>
2018-12-07 12:32:37 +0000

    Fri Dec 07 12:32:37 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -16,19 +16,29 @@ public class FizzBuzzSolution {
     		if (fizzBuzzSolution.isDelux(number)) {
         		return "fizz buzz deluxe";
     		}
-    		else {
+    		else if (fizzBuzzSolution.isFakeDelux(number)){
         		return "fizz buzz fake deluxe";
     		}
     	}
-    	else if ( fizzBuzzSolution.isDelux(number)
-    			&& (fizzBuzzSolution.isDivisible(number, 3) || fizzBuzzSolution.isContains(number, "3") )    			
-    			) {
-    		return "fizz deluxe";
+    	else if ( (fizzBuzzSolution.isDivisible(number, 3) || fizzBuzzSolution.isContains(number, "3") ) ) {
+
+    		if (fizzBuzzSolution.isDelux(number)) {
+        		return "fizz deluxe";
+    		}
+    		else if (fizzBuzzSolution.isFakeDelux(number)){
+        		return "fizz fake deluxe";
+    		}
+
     	}
-    	else if ( fizzBuzzSolution.isDelux(number)
-    			&& (fizzBuzzSolution.isDivisible(number, 5) || fizzBuzzSolution.isContains(number, "5") )    			
-    			) {
-    		return "buzz deluxe";
+    	else if (  (fizzBuzzSolution.isDivisible(number, 5) || fizzBuzzSolution.isContains(number, "5") ) ) {
+
+    		if (fizzBuzzSolution.isDelux(number)) {
+        		return "buzz deluxe";
+    		}
+    		else if (fizzBuzzSolution.isFakeDelux(number)){
+        		return "buzz fake deluxe";
+    		}
+
     	}
     	else if ( fizzBuzzSolution.isDelux(number) ) {
     		return "deluxe";
@@ -72,6 +82,29 @@ public class FizzBuzzSolution {
     		return false;
     }
     
+    private boolean isFakeDelux(Integer number) {
+    	
+    	Map<String, String> numbersMap = new HashMap<String, String>();
+    	
+    	if (number <= 10)
+    		return false;
+    	
+    	String inputNumber = number.toString();
+    	
+    	for(int i=0 ; i<inputNumber.length() ; i++) {
+    		numbersMap.put(inputNumber.charAt(i) + "", inputNumber.charAt(i) + "");
+    	}
+    	
+    	if (numbersMap.keySet().size() == 1) {
+    		if (number % 2 != 0)
+    			return true;
+    		else
+    			return false;
+    	}
+    	else 
+    		return false;
+    }
+    
     private boolean isDivisible(Integer number, Integer dividend) {
     	if (number % dividend == 0)
     		return true;
@@ -89,4 +122,5 @@ public class FizzBuzzSolution {
     }
 
 }
+
 
