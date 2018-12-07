627d49f472f7444480731d8874d594f2e7ca2a05
sbx_user1060 <sbx_user1060@ip-10-129-108-47.eu-west-2.compute.internal>
2018-12-07 11:12:15 +0000

    Fri Dec 07 11:12:15 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -2,7 +2,6 @@ package befaster.solutions.FIZ;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,4 +21,26 @@ public class FizzBuzzSolutionTest {
         assertThat(fizzBuzz.fizzBuzz(9), equalTo("fizz"));
         
     }
+
+    @Test
+    public void fizzBuzz_multiply_five() {
+
+        assertThat(fizzBuzz.fizzBuzz(10), equalTo("buzz"));
+        
+    }
+
+    @Test
+    public void fizzBuzz_multiply_three_five() {
+
+        assertThat(fizzBuzz.fizzBuzz(15), equalTo("fizz buzz"));
+        
+    }
+
+    @Test
+    public void fizzBuzz_multiply_non_of_them() {
+
+        assertThat(fizzBuzz.fizzBuzz(17), equalTo("17"));
+        
+    }
 }
+
