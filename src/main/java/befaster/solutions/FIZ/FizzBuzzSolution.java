769452656c54e8a5414414a0a89930df70df7eb1
sbx_user1060 <sbx_user1060@ip-10-129-108-47.eu-west-2.compute.internal>
2018-12-07 11:14:25 +0000

    Fri Dec 07 11:14:25 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,11 +1,22 @@
 package befaster.solutions.FIZ;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+
+    	if (number.intValue() % 3 == 0 && number.intValue() % 5 == 0) {
+    		return "fizz buzz";
+    	}
+    	else if (number.intValue() % 3 == 0) {
+    		return "fizz";
+    	}
+    	else if (number.intValue() % 5 == 0) {
+    		return "buzz";
+    	}
+    	else {
+    		return Integer.toString(number);
+    	}
     }
 
 }
+
